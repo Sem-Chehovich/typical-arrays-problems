2219becb5962ed8fbb096e6acac20a5d72f5c551
Sem-Chehovich <semchehovich@gmail.com>
2021-07-22 22:11:13 +0300

    init implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)||array.length===0) {
+    return 0;
+  }
+  let arrayTest = array.slice();
+  arrayTest.sort((a, b) => a - b);
+  return arrayTest[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)||array.length===0) {
+    return 0
+  }
+  let arrayTest = array.slice();
+  arrayTest.sort((a, b) => b - a);
+  return arrayTest[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)||array.length===0) {
+    return 0
+  }
+  let arrayTest = array.slice();
+  let sum = 0;
+  for (let i = 0; i < arrayTest.length; i++) {
+    sum += arrayTest[i]
+  }
+  return sum / arrayTest.length;
+
 }
